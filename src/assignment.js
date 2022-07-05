ee30f75302045569ac2335ffe008feaeed5f31fc
Twalib Nurrein <twalibnurrein07@gmail.com>
2022-07-05 09:58:35 +0300

    Update assignment.js

--- src/assignment.js
+++ src/assignment.js
@@ -4,15 +4,7 @@ const assignment = {};
 // ========================
 
 
-/**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
+
 function sumOfNumbers(arrayOfNumbers) {
   var results =  arrayOfNumbers.reduce((accumulator,previousNumber) =>{
       return accumulator + previousNumber
@@ -20,15 +12,7 @@ function sumOfNumbers(arrayOfNumbers) {
   return results;
 }
 
-/**
- * Challenge - 2
- * 
- * Count all even numbers within an array of numbers.
- * If you did Challenge - 2, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array containing even or non-even numbers
- * @returns number the count of even numbers
- */
+
 function countEvenNumbers(arrayOfNumbers) {
     let addSum=0;
     for(number of arrayOfNumbers){
@@ -40,20 +24,7 @@ function countEvenNumbers(arrayOfNumbers) {
     }
 
 
-/**
- * Challenge - 3
- * 
- * Given an array of numbers representing temperatures in Celsius,
- * convert each number to Fahrenheit and return a new array containing the converted
- * temperatures in Fahrenheit. Decimal figures in the converted values in Fahrenheit should be removed.
- * E.g 51.21 should just be 51 (hint: Math.trunc(...) function)
- * If you did Challenge - 3, remove the comment in the line just after this function
- * 
- * See https://www.thoughtco.com/celcius-to-farenheit-formula-609227 for the conversion formula
- * 
- * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
- * @returns Array the converted temperatures in Fahrenheit
- */
+
 function celsiusToFahrenheit(arrayOfNumbers) {
     return arrayOfNumbers.map(number =>{
         return Math.trunc((number *9/5) + 32) 
